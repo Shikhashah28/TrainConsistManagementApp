befd18531a002f9da3d77dd0dcbf2b41968b177f
ShikhaShah28 <shikha28shah@gmail.com>
2026-04-15 09:17:56 +0530

    Initialization train and display consist summary

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+    public class TrainConsistManagementApp {
+        public static void main(String[] args) {
+
+            System.out.println("=== Train Consist Management App ===");
+            System.out.println("-----------------------------------");
+
+            List<String> trainConsist = new ArrayList<>();
+
+            System.out.println("\nTrain initialized successfully...");
+            System.out.println("Initial Bogie Count : " + trainConsist.size());
+            System.out.println("Current Train Consist : " + trainConsist);
+        }
+    }
+}
