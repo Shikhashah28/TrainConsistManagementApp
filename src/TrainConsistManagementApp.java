fb21271e7e3167325582d52d186fa619eb6ff7b5
ShikhaShah28 <shikha28shah@gmail.com>
2026-04-15 09:55:08 +0530

    Track Unique Bogie id

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,28 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TrainConsistManagementApp {
+
     public static void main(String[] args) {
-        System.out.println(" UC2 - Add Passenger Bogies to Train ");
-        List<String> passengerBogies = new ArrayList<>();
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        System.out.println("===================================");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println("===================================\n");
 
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogies : " + passengerBogies);
+        Set<String> bogies = new HashSet<>();
+        bogies.add("BG104");
+        bogies.add("BG103");
+        bogies.add("BG102");
+        bogies.add("BG101");
+        bogies.add("BG101");
+        bogies.add("BG102");
 
-        passengerBogies.remove("AC Chair");
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
 
-        System.out.println("\nAfter Removing 'AC Chair':");
-        System.out.println("Passenger Bogies : " + passengerBogies);
+        System.out.println("\nNote:");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
 
-        System.out.println("\nChecking if 'Sleeper' exists:");
-        boolean exists = passengerBogies.contains("Sleeper");
-        System.out.println("Contains Sleeper? : " + exists);
-
-        System.out.println("\nFinal Train Passenger Consist:");
-        System.out.println(passengerBogies);
-        System.out.println("\nUC2 operations completed successfully...");
+        System.out.println("\nUC3 uniqueness validation completed...");
     }
 }
