afa4631af4bfd6cc5d44e3171d4f0895130b84e6
ShikhaShah28 <shikha28shah@gmail.com>
2026-04-15 11:02:21 +0530

    Safe cargo assignment

--- Test/TrainConsistManagementAppTest.java
+++ Test/TrainConsistManagementAppTest.java
@@ -4,22 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class TrainConsistManagementAppTest extends TrainConsistManagementApp {
 
     @Test
-    void testValidCapacity() throws InvalidCapacityException {
-        PassengerBogie bogie = new PassengerBogie("Sleeper", 72);
-        assertEquals(72, bogie.capacity);
+    void testSafeAssignment() {
+        GoodsBogie bogie = new GoodsBogie("Cylindrical");
+        bogie.assignCargo("Petroleum");
+
+        assertEquals("Petroleum", bogie.cargo);
     }
 
     @Test
-    void testNegativeCapacityThrowsException() {
-        assertThrows(InvalidCapacityException.class, () -> {
-            new PassengerBogie("AC Chair", -5);
-        });
+    void testUnsafeAssignment() {
+        GoodsBogie bogie = new GoodsBogie("Rectangular");
+        bogie.assignCargo("Petroleum");
+
+        assertNull(bogie.cargo); // assignment should fail
     }
 
     @Test
-    void testZeroCapacityThrowsException() {
-        assertThrows(InvalidCapacityException.class, () -> {
-            new PassengerBogie("First Class", 0);
-        });
+    void testProgramContinuation() {
+        GoodsBogie bogie = new GoodsBogie("Rectangular");
+
+        assertDoesNotThrow(() -> bogie.assignCargo("Petroleum"));
     }
 }
